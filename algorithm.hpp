94fb630587c9d815a7c5232017fbf9626d3108ca
Gautam Sharma <55221455+gautam-sharma1@users.noreply.github.com>
2020-11-21 00:16:42 -0700

    Cleaned up the code.
    Removed redundant spaces and improved documentation

--- algorithm.hpp
+++ algorithm.hpp
@@ -32,20 +32,11 @@
 #include <algorithm>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
-
 #include <opencv2/videoio.hpp>
-
 #include <opencv2/objdetect.hpp>
-
 #include <opencv2/imgproc.hpp>
 #include "draw.hpp"
 using namespace cv;
-
-// struct Point
-// {
-//     int x_coord{0};
-//     int y_coord{0};
-// };
 
 
 //Main data structure of the problem
@@ -62,7 +53,8 @@ struct my_point
 };
 
 /*
-Heuristic funtion. Should be modified according to the problem.
+    Heuristic can be changed based on the problem.
+    Current heuristic is (x1-x2)^2 + (y1-y2)^2
 */
 double Heuristic(const PointI &a, const PointI &b)
 {
@@ -97,42 +89,30 @@ public:
         std::cout << "Starting position set to "
                   << "{" << std::get<0>(_starting_pos) << ", " << std::get<1>(_starting_pos) << "} " << std::endl;
     }
-
+    
     /*
-    Heuristic can be changed based on the problem.
-    Current heuristic is (x1-x2)^2 + (y1-y2)^2
+    Checks if the move is valid and implemets the move
     */
-    // double Heursitic(PointI a, PointI b)
-    // {
-    //     return std::sqrt((std::get<0>(a) - std::get<0>(b)) ^ 2 + (std::get<1>(a) - std::get<1>(b)) ^ 2);
-    // };
-
     void Movement(PointI &p)
     {
         _cost += 1;
         if (IsLegal(p))
         {
             std::vector<std::vector<int>> poi{optimal_path[p]};
-            for (auto &x : _moves)
+            for (auto &x : _moves) // For moves in x direction
             {
-                for (auto &y : _moves)
+                for (auto &y : _moves) // For moves in y direction
                 {
-                    if (abs(x) != abs(y))
+                    if (abs(x) != abs(y)) // Can't move diagonally
                     {
                         PointI p_new = std::make_tuple(std::get<0>(p) + x, std::get<1>(p) + y);                       
-
-
-
                         if (IsLegal(p_new) && std::find(_history.begin(), _history.end(), p_new) == _history.end())
-
-                        {
-                            
+                        {  
                             poi.push_back({std::get<0>(p), std::get<1>(p)});
                             optimal_path[p_new] = poi;
                             my_point temp;
                             temp.p = p_new;
                             temp.cost += _cost;
-
                             PQueue.push(temp);
                             _history.push_back(p_new);
                         }
@@ -141,27 +121,22 @@ public:
             }
         }
     }
-
+    
+    /*
+    Checks if the move is within bounds. Boundary is defined from [0,10]
+    */
     bool IsLegal(PointI point)
     {
         return std::get<0>(point) >= 0 && std::get<0>(point) <= 10 && std::get<1>(point) >= 0 && std::get<1>(point) <= 10;
     }
 
-    void SetObstacle(){};
-    void GetObstacle(){};
-
+    void SetObstacle(){}; // Setter function
+    void GetObstacle(){}; // Getter function
+    
+    // Implements the main algorithm and calls openCV functions
     void Implementation()
     {
         Mat Picture(320, 240, CV_8UC3, cv::Scalar(0, 0, 0));
-        //  // Check for failure
-        // if (Picture.empty())
-        // {
-        //     std::cout << "Could not instantiate the image" << std::endl;
-        //     std::cin.get(); //wait for any key press
-        // }
-        // resize(Picture, Picture, Size(600, 600));
-
-        // circle(Picture, Point(std::get<0>(goal)*30, std::get<1>(goal)*30), 20, Scalar(255, 255, 0), 2, 8, 0);
         Draw d(Picture);
         Movement(_starting_pos);
         my_point temp = PQueue.top();
@@ -169,32 +144,15 @@ public:
         {
             temp = PQueue.top();
             d.Simulation(temp.p);
-            //auto x = std::get<0>(temp.p)*30;
-            //auto y = std::get<1>(temp.p)*30;
-            // circle(Picture, Point(x, y), 20, Scalar(0, 255, 0), 2, 8, 0);
-            // imshow("A* Search", Picture);
-            // waitKey(50);
-
-
             PQueue.pop();
             Movement(temp.p);
             std::cout << std::get<0>(temp.p) << " " << std::get<1>(temp.p) << std::endl;
-
             std::vector<std::vector<int>> path = optimal_path[temp.p];
             std::vector<std::vector<int> >::iterator itr = std::unique(path.begin(), path.end());
             path.resize(std::distance(path.begin(), itr));
-            // if(x/30==std::get<0>(goal) && y/30==std::get<1>(goal)){
-            // for (auto itr = path.begin(); itr != path.end(); ++itr)
-            // {
-            //     circle(Picture, Point((*itr).at(0)*30, (*itr).at(1)*30), 20, Scalar(0, 0, 255), 2, 8, 0);
-            //     waitKey(100);
-            //     imshow("A* Search", Picture);
-            // }
 
-            // } 
             for (auto itr = path.begin(); itr != path.end(); ++itr)
             {
-
                 std::cout<<"{"<<(*itr).at(0)<<","<<(*itr).at(1)<<"} ";
             }
             std::cout<<std::endl;
