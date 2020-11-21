eda4fc298443918ca23f2b5a6a038a7bd73a06f5
Gautam Sharma <55221455+gautam-sharma1@users.noreply.github.com>
2020-11-21 00:31:43 -0700

    Improved documentation

--- main.cpp
+++ main.cpp
@@ -2,14 +2,19 @@
 #include "Astar.hpp"
 #include "draw.hpp"
 
-
+/*
+Aim of this project is to abstract away all the complicated stuff to give the user a simple yet efficient interface to implement A* search algorithm.
+*/
 int main()
 {   
-    std::cout << "hello" << std::endl;
-    Astar A(0,3);
-    A.SetGoal(9,9);
-    A.Implementation();
-    //auto [goal_x , goal_y] = A.goal;
-    //std::cout<<goal_x;
+    std::cout << "Hello" << std::endl;
+    Astar A(0,3); // sets starting point of the agent
+    A.SetGoal(9,9); // sets goal coordinate
+    A.Implementation(); // begins the search procedure
+    
+    /*
+    That's it :)
+    */
+    
     return 0;
 }
